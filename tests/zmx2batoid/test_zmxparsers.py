a5bd2ad5be3889a8391ce6b9dfb2a199c3a39bd1
PierreNOptics <pierenicolas@arizona.edu>
2025-06-18 08:55:24 -0700

    Deletion of old files

--- tests/zmx2batoid/test_zmxparsers.py
+++ tests/zmx2batoid/test_zmxparsers.py
@@ -2,7 +2,7 @@ import pathlib
 
 import pytest
 
-from zmx2batoid.zmx_parsers_old import PrescriptionDataParser, ZemaxFileParser
+from zmx2batoid.zmx_parsers import PrescriptionDataParser
 
 TEST_SUPPORT_DATA_DIR = pathlib.Path(__file__).parents[1].joinpath("test_data")
 
@@ -10,19 +10,6 @@ TEST_FILE_ZMX = TEST_SUPPORT_DATA_DIR.joinpath("Ultramarine_Mark-11_DKim1_Releas
 TEST_FILE_PRD = TEST_SUPPORT_DATA_DIR.joinpath(
     "Ultramarine_Mark-11_DKim1_Release_HChoi02_prescriptiondata.txt"
 )
-
-
-def test_ZemaxFileParser():
-    telescope = ZemaxFileParser(TEST_FILE_ZMX, encoding="utf-8")
-
-    assert telescope.surfaces["7"].COMM == "3000mm CA dia. M1"
-    assert telescope.surfaces["9"].COMM == "380X220mm CA M3"
-
-    answer = [190 * 2, 110 * 2]
-    for i, a in enumerate(answer):
-        assert telescope.surfaces["9"].SQAP[i] == a  # multiplied by 2 manually FIXME: WHY???
-
-    assert pytest.approx(3000.0) == telescope.system_details.ENPD
 
 
 def test_PrescriptionDataParser():
